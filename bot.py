fa3182a658c4e4f471bd3128c04dc7f47dcf91af
artski84 <artem.chmulev@gmail.com>
2020-11-11 20:23:48 +0000

    Adding a test comment

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route('/bot', methods=['POST'])
 def bot():
+    # Adding a test comment
     incoming_msg = request.values.get('Body', '').lower()
     resp = MessagingResponse()
     msg = resp.message()
